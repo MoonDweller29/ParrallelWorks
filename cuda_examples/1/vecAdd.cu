81b30cc2f3d4ea9b885fd55bfcb45598d23c8061
Andrew Astapov <andrey.astapov@graphics.cs.msu.ru>
2020-12-26 10:24:46 +0300

    Cuda Examples: async constant set

--- cuda_examples/1/vecAdd.cu
+++ cuda_examples/1/vecAdd.cu
@@ -20,7 +20,7 @@ __global__ void addVector(float* left, float* right, float* result)
 
 void addVector(float* left, float* right, float* result, int SIZE, const Stream& stream) {
 	int curr_mult = 2;
-    SAFE_CALL( cudaMemcpyToSymbol(mult, &curr_mult, sizeof(int)) )
+    SAFE_CALL( cudaMemcpyToSymbolAsync(mult, &curr_mult, sizeof(int), 0, cudaMemcpyHostToDevice,*stream) )
 
     dim3 gridSize = dim3(SIZE/512, 1, 1);    //Размер используемого грида
     dim3 blockSize = dim3(512, 1, 1); //Размер используемого блока

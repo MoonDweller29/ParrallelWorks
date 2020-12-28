b8dca9827c592342e345c0a6dc859237e7dd7fe9
Andrew Astapov <andrey.astapov@graphics.cs.msu.ru>
2020-12-28 15:25:54 +0300

    task3: update scripts for polus

---
+++ taks_3/polus_scripts/compile.sh
@@ -0,0 +1,14 @@
+module load SpectrumMPI
+
+nvcc -ccbin mpixlC -O0 \
+	src/main.cu \
+	src/Mat3D.cu \
+	src/Config.cpp \
+	src/Solver.cu \
+	src/U4D.cpp \
+	src/F3D_f4.cpp \
+	src/cuda_utils/Stream.cu \
+	src/cuda_utils/Event.cu \
+	src/cuda_utils/CudaVec.cu \
+	src/CudaSolver.cu \
+	 -o main.out
